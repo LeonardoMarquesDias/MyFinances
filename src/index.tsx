c11d2733b5937209f7470a8d768de92de892dab6
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-08-26 04:47:19 +0100

    Miragejs fakeApi

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
 
+createServer({
+  routes() {
+    this.namespace = 'api'; //todas chamandas api vai ser apartir desde endereço /api/
+
+    this.get('/transactions', () => { //get é requisão de busca, buscando alguma infomação
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date() //data de criação
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
