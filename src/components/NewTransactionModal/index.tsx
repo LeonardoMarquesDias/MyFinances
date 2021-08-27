270f7f6f83daeb60a6bfdb71ac2086d8a3551440
LeonardoDias <leonardo_marquesdias@hotmail.com>
2021-08-27 03:42:02 +0100

    ModalContent/Close

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import { Container } from './styles';
+
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +13,37 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     <Modal 
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay" 
+      className="react-modal-content"
     >
-      <h1>nova transação</h1>
+      <button 
+        type="button" 
+        onClick={onRequestClose} 
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="" />
+      </button>
+     
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input 
+          placeholder="Título"
+        />
+
+        <input 
+          type="number" 
+          placeholder="Valor"
+        />
+
+        <input
+          placeholder="Categoria" 
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   )
 }
